5cd54ac90205087cdc92d34cbc88241a30991a99
antoine lafitte <antoine.lafitte99411@gmail.com>
2024-09-04 15:59:29 +0200

    fix elif statement

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -6,7 +6,7 @@ last_dgt = abs(number) % 10 if number >= 0 else -(abs(number) % 10)
 
 if last_dgt > 5:
     print(f"Last digit of {number} is {last_dgt} and is greater than 5")
-elif last_digit == 0:
+elif last_dgt == 0:
     print(f"Last digit of {number} is {last_dgt} and is 0")
 else:
     print(f"Last digit of {number} is {last_dgt} and is less than 6 and not 0")
