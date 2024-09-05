4c23224a3eb8925c3cedcca911cce4f70ba8450c
antoine lafitte <antoine.lafitte99411@gmail.com>
2024-09-05 23:48:43 +0200

    fullfilling requirements

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
     import hidden_4
 
-    for name in dir(hidden_4):
-        if name[0:2] != "__":
-            print("{}" .format(name))
+    # Get all names inside hidden_4
+    names = dir(hidden_4)
+    
+    # Filter and print names that don't start with "__"
+    for name in sorted(names):
+        if not name.startswith("__"):
+            print(name)
