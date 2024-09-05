ec9402087b9cc7e9b2f1ebaf4d4694715a0ce1e2
antoine lafitte <antoine.lafitte99411@gmail.com>
2024-09-06 00:13:35 +0200

    Add 4-hidden_discovery.py to the repo

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -2,9 +2,9 @@
 if __name__ == "__main__":
     import hidden_4
 
-    # Get all names inside hidden_4
+    # Get all names defined in hidden_4 module
     names = dir(hidden_4)
-    
+
     # Filter and print names that don't start with "__"
     for name in sorted(names):
         if not name.startswith("__"):
