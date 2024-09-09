73aaf7966328082d9a09d7faf327647831a0ed65
antoine lafitte <antoine.lafitte99411@gmail.com>
2024-09-09 14:23:35 +0200

    5-no_c.py done

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
     modified_string = ""
-    for char in modified_string:
+
+    for char in my_string:
         if char != 'c' and char != 'C':
             modified_string += char
     return modified_string
