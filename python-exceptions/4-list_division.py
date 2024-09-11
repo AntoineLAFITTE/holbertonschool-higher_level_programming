d1e0376fdc4c910fcfa7e27074e5dd3546a05dbc
antoine lafitte <antoine.lafitte99411@gmail.com>
2024-09-11 16:51:25 +0200

    adding variables for fun maybe it will change something

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -4,7 +4,9 @@ def list_division(my_list_1, my_list_2, list_length):
 
     for i in range(list_length):
         try:
-            result = my_list_1[i] / my_list_2[i]
+            num1 = my_list_1[i]
+            num2 = my_list_2[i]
+            result = num1 / num2
         except IndexError:
             print("out of range")
             result = 0
