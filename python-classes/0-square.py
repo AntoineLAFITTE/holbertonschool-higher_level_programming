fb9bda10233e4ff91349838c32ee2baf4af3a34d
antoine lafitte <antoine.lafitte99411@gmail.com>
2024-09-16 11:05:32 +0200

    0-square.py documentation friendly

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+"""
+This module defines an empty class Square.
+"""
+
+
 class Square:
     """
-    An empty class that defines a square.
-
+    This class defnies a square.
     This class currently does not have any attributes or methods.
     """
     pass
