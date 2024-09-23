122af301f20b036bff9b24c17f1883deb6579cfa
antoine lafitte <antoine.lafitte99411@gmail.com>
2024-09-23 12:09:21 +0200

    0-lookup.py documentation

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""This function Returns the list of attributes and methods of an object"""
+
+
 def lookup(obj):
-   """Returns the list of atributes and methods of an object"""
+   """Returns the list of attributes and methods of an object"""
    return dir(obj)
