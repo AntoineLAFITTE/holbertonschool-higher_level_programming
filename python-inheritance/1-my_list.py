7a982bfbafd8ea238b4c7571dfe87cc040fce76c
antoine lafitte <antoine.lafitte99411@gmail.com>
2024-09-23 17:36:11 +0200

    1-my_list.py done

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" MyList class wich inherits from list"""
+
+
+class MyList(list):
+    """A class wich inherits from list and adds a method to print the list
+    sorted"""
+
+    def print_sorted(self):
+        """
+        Prints the list, but sorted in ascending order.
+        """
+        print(sorted(self))
