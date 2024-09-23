341112d9071d9ad310a049c76e4071e1ed071631
antoine lafitte <antoine.lafitte99411@gmail.com>
2024-09-23 22:29:10 +0200

    10-square.py pushes to check

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""This module defines a class Square that inherits from Rectangle."""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+class Square(Rectangle):
+    """A class representing a square, inheriting from Rectangle."""
+
+    def __init__(self, size):
+        """Initialize a new Square instance.
+
+        Args:
+            size (int): The size of the square (both width and height).
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """Returns the area of the square."""
+        return self.__size * self.__size
