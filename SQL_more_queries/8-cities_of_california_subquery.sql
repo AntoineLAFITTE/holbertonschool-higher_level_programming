0b774ab48b1562ef827f16674dd757319238bae3
antoine lafitte <antoine.lafitte99411@gmail.com>
2024-10-31 14:48:39 +0100

    8-cities_of_california_subquery.sql script added

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,4 @@
 SELECT id, name
 FROM cities
-WHERE state_id = (SELECT id FROM states WHERE name = 'California')
+WHERE state_id = (SELECT id FROM hbtn_0d_usa.states WHERE name = 'California')
 ORDER BY id ASC;
