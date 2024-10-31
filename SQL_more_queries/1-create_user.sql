54f31ce740cbbbd96d81296d1ade002549577d69
antoine lafitte <antoine.lafitte99411@gmail.com>
2024-10-31 13:04:42 +0100

    1-create_user.sql adding password to user

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,8 +1,5 @@
 --  This script creates user user_0d_1 in the MySQL server
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_pwd';
 
 -- Grant all privileges to user_0d_1 on the server
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-
--- Apply changes;
-FLUSH PRIVILEGES;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
