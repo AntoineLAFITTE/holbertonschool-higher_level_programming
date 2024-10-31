713efbee501fb7d19ce9ea94c77bdfe8faf1e64a
antoine lafitte <antoine.lafitte99411@gmail.com>
2024-10-31 14:18:07 +0100

    Should work as entended now

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,7 +1,7 @@
 -- This script creates the database hbtn_0d_usa if doesn't already exist
 -- Uses the folowing database
 -- Creates the table cities with a foreign key reference to the states table
-CREATE DATBASE IF NOT EXISTS hbtn_0d_usa;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
   id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
   state_id INT NOT NULL,
