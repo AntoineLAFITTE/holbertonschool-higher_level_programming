1c7eaae83d056196c09202768a721ea20fbc6d36
antoine lafitte <antoine.lafitte99411@gmail.com>
2024-10-31 14:14:46 +0100

    7-cities.sql also done by me and only me im so independent once again thanks Anzo

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -3,8 +3,8 @@
 -- Creates the table cities with a foreign key reference to the states table
 CREATE DATBASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
-  id INT AUTO_INCREMENT cities KEY,
+  id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
   state_id INT NOT NULL,
   name VARCHAR(256) NOT NULL,
-  FOREIGN KEY (state_id) REFERENCE states(id)
+  FOREIGN KEY (state_id) REFERENCES states(id)
 );
