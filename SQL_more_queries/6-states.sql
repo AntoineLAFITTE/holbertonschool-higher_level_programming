f575dcde5031a52a1b8ecc5b2d9da93793d8f2a8
antoine lafitte <antoine.lafitte99411@gmail.com>
2024-10-31 13:54:57 +0100

    6-states.sql script done

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,6 @@
+-- This script creates the hbtn_0d_usa database and a table named states.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE hbtn_0d_usa.states (
+  id INT AUTO_INCREMENT NOT NULL UNIQUER PRIMARY KEY,
+  name VARCHAR(256) NOT NULL
+);
