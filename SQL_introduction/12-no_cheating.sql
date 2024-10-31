30953c898f44873fbe16f84b6a951579dd78cc29
antoine lafitte <antoine.lafitte99411@gmail.com>
2024-10-31 12:05:32 +0100

    12-no_cheating.sql script done

---
+++ SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,5 @@
+-- This script updates the score of the record with name 'Bob' to 10 in the
+-- table second_table in the specified database.
+UPDATE second_table
+SET score = 10
+WHERE name = 'Bob';
