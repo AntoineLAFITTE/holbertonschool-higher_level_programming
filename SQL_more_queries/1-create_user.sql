2bd337956dd357eb57bc6d5b1d61622b2916c9e0
antoine lafitte <antoine.lafitte99411@gmail.com>
2024-10-31 13:11:32 +0100

    1-create_user.sql fixessss

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
--- This script creates user user_0d_1 in the MySQL server
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_pwd';
+-- Creates user with privileges granted
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
