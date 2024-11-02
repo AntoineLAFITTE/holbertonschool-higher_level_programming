cd492bc90e06d4ace2c9d9e33919e56fb9303806
antoine lafitte <antoine.lafitte99411@gmail.com>
2024-11-02 17:19:48 +0100

    model_state.py pycode fix

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -8,6 +8,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
     """
     State class that links to the 'states' table in MySQL database
